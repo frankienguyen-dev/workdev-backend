abd2757010389405119af050a5476dd766d02aaf
frankienguyen-dev <binguyen221198@gmail.com>
2023-12-07 00:04:02 +0700

    Upload base project

---
+++ src/main/java/com/frankie/workdev/WorkDevApplication.java
@@ -0,0 +1,13 @@
+package com.frankie.workdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frankie/workdev/WorkDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankie.workdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
